f015bf2874e1c9bbe4d6069a5651f3b1550047bd
mochachess <mentos_vist@mail.ru>
2021-04-25 02:22:02 +0300

    first commit

---
+++ src/main/java/controller/PageController.java
@@ -0,0 +1,10 @@
+package controller;
+
+import dto.PageDto;
+
+public class PageController {
+
+    public Long createPage(PageDto ){
+
+    }
+}
---
+++ src/main/java/dto/PageDto.java
@@ -0,0 +1,14 @@
+package dto;
+
+public class PageDto {
+    private Long id;
+    private String content;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
